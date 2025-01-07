8f2e232c1fc6d183a98c535ec5eb90ca9e980637
HwangSukHyeon <payback9488@gmail.com>
2025-01-07 19:02:50 +0900

    proj: 프로젝트 생성
    - 코드베이스 설정
    - SnapKit
    - RxSwift
    - FireBase

---
+++ SwiftRun/SwiftRunTests/SwiftRunTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRunTests.swift
+//  SwiftRunTests
+//
+//  Created by 황석현 on 1/7/25.
+//
+
+import Testing
+@testable import SwiftRun
+
+struct SwiftRunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
