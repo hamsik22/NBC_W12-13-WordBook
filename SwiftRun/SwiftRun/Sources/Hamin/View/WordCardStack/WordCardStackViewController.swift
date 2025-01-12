7a92a18746ee348bbbeb922fa4a8b5f3ee32bf6b
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-01-13 01:08:55 +0900

    mod: wordCardView를 드래그하면 움직이고, 놓으면 기존 자리로 돌아오게 하는 기능 구현

--- SwiftRun/SwiftRun/Sources/Hamin/View/WordCardStack/WordCardStackViewController.swift
+++ SwiftRun/SwiftRun/Sources/Hamin/View/WordCardStack/WordCardStackViewController.swift
@@ -13,6 +13,8 @@ final class WordCardStackViewController: UIViewController {
     let viewModel = DummyViewModel()
     let wordCardView = WordCardView()
     
+    private var originalPosition = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +22,15 @@ final class WordCardStackViewController: UIViewController {
         wordCardView.bind(to: viewModel)
         view.addSubview(wordCardView)
         setConstraints()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        wordCardView.addGestureRecognizer(panGesture)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        originalPosition = wordCardView.center
     }
     
     
@@ -28,5 +39,36 @@ final class WordCardStackViewController: UIViewController {
             make.edges.equalTo(view.safeAreaLayoutGuide).inset(16)
         }
         
+    }
+}
+
+// MARK: - Pan Gesture handler
+
+extension WordCardStackViewController {
+    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
+        guard let viewToDrag = gesture.view else { return }
+        
+        let translation = gesture.translation(in: view)
+        print("original position: " + self.originalPosition.debugDescription)
+
+        switch gesture.state {
+        case .began: print("Begined dragging")
+        case .changed:
+            viewToDrag.center = CGPoint(x: viewToDrag.center.x + translation.x,
+                                        y: viewToDrag.center.y + translation.y)
+            
+            gesture.setTranslation(.zero, in: view)
+            print("Position changed")
+        case .ended:
+            print("\(self.originalPosition)")
+            UIView.animate(withDuration: 0.1) {
+                viewToDrag.center = self.originalPosition
+            }
+            print("Ended dragging")
+        case .cancelled: print("Cancelled dragging")
+        case .possible: print("Ready to drag")
+        case .failed: print("Failed")
+        @unknown default: print("Unknown state")
+        }
     }
 }
