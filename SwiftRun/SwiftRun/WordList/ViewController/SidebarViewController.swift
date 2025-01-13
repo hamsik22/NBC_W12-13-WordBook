f28afb764022075353965e1911b2fc44901f55d5
dnwn1211 <128039034+dnwn1211@users.noreply.github.com>
2025-01-13 18:00:58 +0900

    feat: 사이드바 목록 가져오기

--- SwiftRun/SwiftRun/WordList/ViewController/SidebarViewController.swift
+++ SwiftRun/SwiftRun/WordList/ViewController/SidebarViewController.swift
@@ -5,12 +5,13 @@ class SidebarViewController: UIViewController {
     // 클로저를 정의
     var onItemSelected: ((String) -> Void)?
 
-    private let items = ["항목 1", "항목 2", "항목 3"]
+    private var items: [String] = []
     private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        fetchCategories()
     }
 
     private func setupUI() {
@@ -24,6 +25,39 @@ class SidebarViewController: UIViewController {
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+    }
+
+    private func fetchCategories() {
+        guard let url = URL(string: "https://iosvocabulary-default-rtdb.firebaseio.com/categories.json") else {
+            print("잘못된 URL입니다.")
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            if let error = error {
+                print("데이터 요청 실패: \(error.localizedDescription)")
+                return
+            }
+
+            guard let data = data else {
+                print("데이터가 없습니다.")
+                return
+            }
+
+            do {
+                // JSON 데이터를 Dictionary 형태로 디코딩
+                let categoriesDict = try JSONDecoder().decode([String: Category].self, from: data)
+                let names = categoriesDict.values.map { $0.name }.sorted()
+
+                DispatchQueue.main.async {
+                    self?.items = names
+                    self?.tableView.reloadData() // 테이블뷰 갱신
+                }
+            } catch {
+                print("디코딩 실패: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
     }
 }
 
