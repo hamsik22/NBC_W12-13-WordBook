6d95c7cb63e073f355b9d24164ca30d034480ef5
soungjunban <soungjunban@gmail.com>
2025-01-14 20:01:17 +0900

    update:Refactor WordBookCollectionCell

--- SwiftRun/SwiftRun/Home/View/WordBookCollectionCell.swift
+++ SwiftRun/SwiftRun/Home/View/WordBookCollectionCell.swift
@@ -10,39 +10,48 @@ import SnapKit
 
 class WordBookCollectionCell: UICollectionViewCell {
     
-    static let identifier = "wordBookCollectionCell"
+    static let identifier = String(describing: WordBookCollectionCell.self)
     
-    let wordBookTitle: UILabel = {
+    private let wordBookTitle: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
         label.textColor = UIColor(named: "SRBlue400")
         label.layer.borderWidth = 2
         label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
         label.layer.borderColor = UIColor(named: "SRBlue400")?.cgColor
         label.numberOfLines = 2
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.adjustsFontForContentSizeCategory = true
         return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(wordBookTitle)
-        
-        wordBookTitle.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.size.equalToSuperview()
-        }
+        configureUI()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func configureUI() {
+        addSubview(wordBookTitle)
+    }
+    
+    private func setupConstraints() {
+        wordBookTitle.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(8)
+        }
+    }
+    
     func configure(with item: Category) {
-        wordBookTitle.text = item.name
+        wordBookTitle.text = item.name.isEmpty ? "Unnamed Category" : item.name
     }
 }
 
 @available(iOS 17.0, *)
-#Preview("HomeViewController") {
-    HomeViewController()
+#Preview("WordBookCollectionCell") {
+    WordBookCollectionCell()
 }
